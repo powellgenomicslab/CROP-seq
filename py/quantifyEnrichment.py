91d971cc419560e29570773cba99f74614f17144
William Colgan <colgan.william@gmail.com>
2020-11-17 14:46:58 -0500

    data frame preallocation to speed up quantifyEnrichment

--- py/quantifyEnrichment.py
+++ py/quantifyEnrichment.py
@@ -2,21 +2,29 @@
 import argparse
 import pysam
 import pandas as pd
+import numpy as np
 
 def quantifyGuides(x, bam_input = None):
     columns = ["gRNA", "cell_barcode", "umi", "reads"]
     molecule_df = pd.DataFrame(columns = columns)
+    row_df = pd.DataFrame({"gRNA":np.chararray(10000),"cell_barcode":np.chararray(10000), \
+        "umi":np.chararray(10000),"reads":np.zeros(10000)})
+    i = 0
 
     for read in bam_input.fetch(x + "_chrom"):
         # Parse read to get barcodes
         tag = (read.query_name).split(":")[-1]
         cell_barcode = tag[0:16]
         umi = tag[16:-1]
-        
-        row_df = pd.DataFrame([(x, cell_barcode, umi, 1)], columns = columns) 
-        molecule_df = molecule_df.append(row_df, ignore_index = True)
+        row_df.iloc[i % 10000,:] = [x, cell_barcode, umi, 1]
+        i += 1
 
-    return(molecule_df)        
+        if i % 10000 == 0:
+            molecule_df = molecule_df.append(row_df, ignore_index = True)
+            row_df = pd.DataFrame({"gRNA":np.chararray(10000),"cell_barcode":np.chararray(10000), \
+                "umi":np.chararray(10000),"reads":np.zeros(10000)})
+
+    return(molecule_df.iloc[:i-1,:])
 
 def parseArgs():
     parser = argparse.ArgumentParser()
@@ -38,7 +46,7 @@ if __name__ == "__main__":
 
     # Gather cell barcodes
     bam_input = pysam.AlignmentFile(bam_filepath, "rb")
-    guide_df_list = [quantifyGuides(x, bam_input = bam_input) for x in guide_df["sgRNA"].tolist() ] 
+    guide_df_list = [quantifyGuides(x, bam_input = bam_input) for x in guide_df["sgRNA"].tolist() ]
     bam_input.close()
 
     molecule_df = pd.concat(guide_df_list)
